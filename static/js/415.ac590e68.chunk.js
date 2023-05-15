235d84762e5ede63b26af40c915febbde5005cfb
savchyndd <96209694+savchyndd@users.noreply.github.com>
2023-05-15 20:13:19 +0000

    Deploying to gh-pages from @ savchyndd/goit-react-hw-05-movies@8afe97d97ee512f51dd23b020c484d298099d66c 🚀

---
+++ static/js/415.ac590e68.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[415],{415:function(e,t,i){i.r(t);var c=i(184);t.default=function(){return(0,c.jsx)("div",{children:"Home"})}}}]);
+//# sourceMappingURL=415.ac590e68.chunk.js.map
